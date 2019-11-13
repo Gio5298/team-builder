170975906158674c1d091a17fd4f8adb7bd976ce
Regino P <regino5298@gmail.com>
2019-11-13 13:29:35 -0700

    init commit, ITINAJJ

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Identity theft is not a joke Jim!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
