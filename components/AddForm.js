42a05af8b6eb1f6dd8dbefad20b4869115ea4192
Regino P <regino5298@gmail.com>
2019-11-13 15:25:47 -0700

    added AddForm info

--- components/AddForm.js
+++ components/AddForm.js
@@ -1,5 +1,21 @@
 import React, {useState} from 'react';
 
 const AddForm = props => {
-    const [from, setForm] = useState()
+    return(
+        <Card>
+            <Title>{props.info.name}</Title>
+            <CardContent>
+                <CardImage src={props.info.image}/>
+                <div>
+                    <div>
+                        ID: {props.info.id}
+                    </div>
+                    <div>
+                        Position: {props.info.role}
+                    </div>
+                </div>
+            </CardContent>
+        </Card>
+    )
 }
+export default AddForm;
---
+++ src/data.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'Solid Snake',
+        role: 'Front-end Engineer',
+        image: <img className='solid' src='img/Solid-Snake.jpg' alt='Solid from mgs2' />
+    },
+    {
+        id: 2,
+        name: 'Liquid Snake',
+        role: 'Back-end Engineer',
+        image: <img className='liquid' src='img/Liquid-Snake.png' alt='Liquid from mgs1'/>
+    },
+    {
+        id: 3,
+        name: 'Solidus Snake',
+        role: 'UX Designer',
+        image: <img className='solidus' src= 'img/Solidus-Snake.jpg' alt='Solidus from mgs2'/>
+    },
+]
+
+export default members 
